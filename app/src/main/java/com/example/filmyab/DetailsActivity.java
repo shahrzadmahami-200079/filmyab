e3977244e1648ebb4bfad31763e3b1d2768499b6
shahrzadmahami-20007 <mahamishahrzad99@gmail.com>
2020-06-28 18:08:21 +0430

    complete DtailsActivity

--- app/src/main/java/com/example/filmyab/DetailsActivity.java
+++ app/src/main/java/com/example/filmyab/DetailsActivity.java
@@ -6,13 +6,48 @@ import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class DetailsActivity extends AppCompatActivity {
+import com.bumptech.glide.Glide;
 
+public class DetailsActivity extends AppCompatActivity {
+    private TextView tvMovieTitle;
+    private TextView tvMovieDescription;
+    private ImageView ivMovieImage;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        setData();
     }
+
+    void setData() {
+        // get Data
+        String MovieName = getIntent().getExtras().getString("title");
+        String MovieSummery = getIntent().getExtras().getString("overview");
+        String MoviePoster = getIntent().getExtras().getString("backdrop_path");
+
+        // set movie poster selection
+        ivMovieImage = findViewById(R.id.d_imageview);
+        Glide.with(this).load(MoviePoster).into(ivMovieImage);
+
+        // set movie title selection
+        tvMovieTitle = findViewById(R.id.d_title);
+        tvMovieTitle.setText(MovieName);
+
+        // set movie summery selection
+       tvMovieDescription = findViewById(R.id.d_description);
+       tvMovieDescription.setText(MovieSummery);
+
+
+
+
+        //set movie name of header detail activity
+        getSupportActionBar().setTitle(MovieName);
+
+
+
+    }
+
 }
